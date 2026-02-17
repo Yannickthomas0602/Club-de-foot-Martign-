1cc1483869b1de060a4c59e7a0b2656e00cb8be6
timeo-grrd <timeog53@gmail.com>
2026-02-18 00:12:17 +0100

    ajout de la page résultat avec le widget de score n'co

---
+++ resultats.php
@@ -0,0 +1,9 @@
+<?php include "header.php"; ?>
+<div class="scorenco-widget" data-widget-type="team" data-widget-id="168255" style="background: #F2F5F9; height: 500px; display: flex; align-items: center; justify-content: center; flex-direction: column;text-transform: uppercase; font-family: sans-serif; font-weight: bolder; gap: 9px; color:#1E457B;">
+  <div class="ldsdr"></div>
+  Score'n'co - Cadets Chelun Martigné
+  </div>
+  <style>.ldsdr{display:inline-block;width:80px;height:80px}.ldsdr:after{content:" ";display:block;width:64px;height:64px;margin:8px;border-radius:50%;border:6px solid #1E457B;border-color:#1E457B transparent;animation:ldsdr 1.2s linear infinite}@keyframes ldsdr{0%{transform:rotate(0deg)}100%{transform:rotate(360deg)}}</style>
+  <script async defer src="https://widgets.scorenco.com/host/widgets.js"></script>
+
+   <?php include "footer.php"; ?>
