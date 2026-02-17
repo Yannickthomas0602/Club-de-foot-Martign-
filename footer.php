4e98936e455ebcce5dfee05a6af02f9a62bc4d4d
timeo-grrd <timeog53@gmail.com>
2026-02-17 01:23:08 +0100

    modification du header, pas de responsive !

--- footer.php
+++ footer.php
@@ -5,31 +5,53 @@
 <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Great+Vibes&family=Pacifico&family=Roboto:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
-        <div class="social_media">
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
 
+        <?php include 'carousel.php'; ?>
+        <script src="assets/js/carousel.js"></script>
+        <div class="footer-yellow-line"></div>
 
-
-        <div class="rectangle_footer">
-        
-            <p>Suivez-nous : </p>
-            <a class="social_link" href="https://www.facebook.com/p/Cadets-Chelun-Martigné-Ferchaud-100057405613179/" target="blank"><img class="social_img" src="assets\img\Logo_club\icons8-facebook-96.png" alt="logo facebook">
-            </a>
-            <a class="social_link" href="https://www.instagram.com/cadetschelunmartigne/" target="blank"><img class="social_img" src="assets\img\Logo_club\icons8-instagram-100 (1).png" alt="logo instagram">
-            </a>
-            <p class="devise">"Jouer Ensemble, Vivre Ensemble, Grandir Ensemble"</p>
-            
-        
-         <?php include 'carousel.php'; ?>
-    
-    <script src="assets/js/carousel.js"></script>
-    <nav class="mentions_legales">
-            <ul>
-                <li><a href="#">Mentions l&eacute;gales</a></li>
-                <li><a href="#">Informations l&eacute;gales</a></li>
-                <li><a href="#">Gestion des cookies</a></li>
+        <div class="rectangle_footer footer-flex">
+          <div class="footer-col footer-left">
+            <p class="devise">
+              Jouer Ensemble,<span class="devise-break"> Vivre Ensemble,</span><span class="devise-break"> Grandir Ensemble</span>
+            </p>
+            <div class="social_links">
+              <a class="social_link" href="https://www.facebook.com/p/Cadets-Chelun-Martigné-Ferchaud-100057405613179/" target="blank"><img class="social_img" src="assets/img/Logo_club/icons8-facebook-100 (1).png" alt="logo facebook"></a>
+              <a class="social_link" href="https://www.instagram.com/cadetschelunmartigne/" target="blank"><img class="social_img" src="assets/img/Logo_club/icons8-instagram-100 (1).png" alt="logo instagram"></a>
+            </div>
+          </div>
+          <div class="footer-col footer-center">
+            <p class="footer-title">Navigation</p>
+            <div class="footer-nav-columns">
+              <ul class="footer-nav">
+                <li><a href="#">Accueil</a></li>
+                <li><a href="#">Résultats</a></li>
+                <li><a href="#">Calendrier</a></li>
+                <li><a href="#">Équipes</a></li>
+                <li><a href="#">Convocations</a></li>
+                <li><a href="photos.php">Album photos</a></li>
+              </ul>
+              <ul class="footer-nav">
+                <li><a href="#">PEF</a></li>
+                <li><a href="#">Horaire et plan</a></li>
+                <li><a href="#">Organigramme</a></li>
+                <li><a class="boutique-link" href="https://cadets-chelun-martigne.kalisport.com/" target="_blank" rel="noopener noreferrer">Boutique</a></li>
+                <li><a href="#">Nos sponsors</a></li>
+                <li><a href="#">Espace coach</a></li>
+              </ul>
+            </div>
+          </div>
+          <div class="footer-col footer-right">
+            <p class="footer-title">Informations légales</p>
+            <ul class="footer-legal">
+              <li><a href="#">Mentions légales</a></li>
+              <li><a href="#">Informations légales</a></li>
+              <li><a href="#">Gestion des cookies</a></li>
             </ul>
-        </nav>
-        </div>
+          </div>
         </div>
         
      </footer>
