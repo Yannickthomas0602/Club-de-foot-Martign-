067ecb8f6a233c210fecffaf20061ef2400c8dec
Yannickthomas0602 <yannick.thomas@supdevinci-edu.fr>
2026-02-27 11:58:52 +0100

    petites modif + tentative organigramme

--- admin.php
+++ admin.php
@@ -12,11 +12,12 @@ $page_title = "Administration";
     <?php include 'header.php'; ?>
     <main>
     <p>Admin</p>
-    <a href="manage_users.php">Gérer les utilisateurs</a>
-    <br>
-    <a href="convocation_admin.php">accès convocation</a>
-    <br>
-    <a href="annonces_popup_admin.php">Gérer les annonces pop-up</a>
+    <li>
+    <ul><a href="manage_users.php">Gérer les utilisateurs</a></ul>
+    <ul><a href="convocation_admin.php">Gérer les convocations</a></ul>
+    <ul><a href="annonces_popup_admin.php">Gérer les annonces pop-up</a></ul>
+    <ul><a href="admin_organigramme.php">Gérer l'organigramme</a></ul>
+    </li>
     </main>
     <footer>
         <?php include 'footer.php'; ?>
